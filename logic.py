1a85062fb433f2fd1ef745840715c20fcbaf8f02
David Safro <87347098+David-Safro@users.noreply.github.com>
2022-06-20 08:15:03 -0400

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,1 +1,32 @@
+import pygame
+
+
+class Tower:
+    def __init__(self, height, width, x, y):
+        self.height = height
+        self.width = width
+        self.x = x
+        self.y = y
+        self.color = "black"
+
+    def draw(self, win):
+        rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        pygame.draw.rect(win, self.color, rect)
+
+
+class Ring:
+    def __init__(self, width, x, y, tower, color):
+        self.width = width
+        self.x = x
+        self.y = y
+        self.color = color
+        self.tower = tower
+
+    def draw(self, win):
+        rect = pygame.Rect(self.x, self.y, self.width, 20)
+        pygame.draw.rect(win, self.color, rect)
+
+    def switch_towers(self, start, end, order, ):
+        if order == 0:
+            self.y -= Tower.height - (650-self.y)
 
